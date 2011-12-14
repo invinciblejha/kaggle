30b9be1cd4cad77880f8e4bef73a2df9b2ad299b
Peter Williams <peter.williams.97@gmail.com>
2011-12-14 22:49:38 +1100

    planning feature selection

---
+++ tools/select_features.py
@@ -0,0 +1,15 @@
+"""
+    Given X,y find the features in X that best predict y where prediction is measured
+    with k-fold cross-validation
+    
+    Chooose a classifier C e.g. logistic regression
+    
+    Rank individual features by prediction
+    
+    For n = 1 .. num features -1
+        Using best n-tuples of features
+        Create n+1 tuples
+        Find best using GA
+        
+
+"""
