5fe1354a6150b929abb2e3945825ee879f67da69
Peter Williams <peter.williams.97@gmail.com>
2011-12-29 17:20:07 +1100

    basic plotting of classification

---
+++ heritage/predict.py
@@ -0,0 +1,227 @@
+"""
+
+"""
+#print __doc__
+
+import math
+import numpy as np
+from scipy import interp
+import pylab as pl
+
+import sklearn
+from sklearn import svm, metrics
+from sklearn.metrics import roc_curve, auc
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.linear_model import SGDClassifier
+from sklearn.cross_validation import StratifiedKFold
+
+import common  
+import select_features
+
+def plot_2d_histo_raw(x0, x1, y, label_x, label_y, x0_len, x1_len):
+    """Plot a 2D histogram
+       
+    """
+    print 'plot_2d_histo_raw(x0=%s, x1=%s, y=%s, label_x=%s, label_y=%s)' % (x0.shape, x1.shape, 
+        y.shape, label_x, label_y)
+
+    n = x0.size
+
+    unique_0 = np.unique(x0)
+    unique_1 = np.unique(x1)
+    print 'unique_0=%s' % unique_0
+    print 'unique_1=%s' % unique_1
+    
+    d0 = x0_len /(unique_0.size + 1)/2.0
+    d1 = x1_len /(unique_1.size + 1)/2.0
+    
+    lut_0 = {}
+    lut_1 = {}
+    for i, u in enumerate(unique_0):
+        lut_0[u] = i
+    for i, u in enumerate(unique_1):
+        lut_1[u] = i
+    
+    counts_pos = np.zeros([len(lut_0), len(lut_1)])
+    counts_neg = np.zeros([len(lut_0), len(lut_1)])
+    
+    # Make histogram
+    for i in range(n):
+        i0 = lut_0[x0[i]]
+        i1 = lut_1[x1[i]]
+        if y[i]:
+            counts_pos[i0,i1] += 1.0/n 
+        else:    
+            counts_neg[i0,i1] += 1.0/n 
+            
+    print 'counts_pos = %s' % counts_pos.sum()      
+    print 'counts_neg = %s' % counts_neg.sum() 
+    
+    for i0, u0 in enumerate(unique_0):
+        for i1, u1 in enumerate(unique_1):
+            r_pos = math.sqrt(counts_pos[i0][i1])/2.0
+            r_neg = math.sqrt(counts_neg[i0][i1])/2.0
+            #print ix,iy,r
+            
+            if r_pos > r_neg:
+                circ = pl.Circle((u0+d0,u1+d1),radius=r_pos,color='red')
+                ax = pl.gca()
+                ax.add_patch(circ)
+                circ = pl.Circle((u0-d0,u1-d1),radius=r_neg,color='blue')
+                ax = pl.gca()
+                ax.add_patch(circ)
+            else:    
+                circ = pl.Circle((u0-d0,u1-d1),radius=r_neg,color='blue')
+                ax = pl.gca()
+                ax.add_patch(circ)
+                circ = pl.Circle((u0+d0,u1+d1),radius=r_pos,color='red')
+                ax = pl.gca()
+                ax.add_patch(circ)
+
+    pl.xlabel(label_x)
+    pl.ylabel(label_y)
+
+def plot_classification(X, y, y_pred, keys, title, clf):
+    print 'plot_classification(X=%s, y=%s, y_pred=%s, keys=%s, title=%s)' % (X.shape, 
+        y.shape, y_pred.shape, keys, title)
+    h = .2 # step size in the mesh
+    
+    # create a mesh to plot in
+    x_min, x_max = X[:,0].min()-1, X[:,0].max()+1
+    y_min, y_max = X[:,1].min()-1, X[:,1].max()+1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    
+    pl.set_cmap(pl.cm.Paired)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, m_max]x[y_min, y_max].
+    #pl.subplot(2, 2, i+1)
+    print 'xx.size=%s, xx.shape=%s, X.shape=%s' % (xx.size, xx.shape, X.shape)
+    points = np.zeros([xx.size, X.shape[1]])
+    points[:,0] = xx.ravel() 
+    points[:,1] = yy.ravel()
+    Z = clf.predict(points)
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    pl.set_cmap(pl.cm.Paired)
+    pl.contourf(xx, yy, Z)
+    pl.axis('tight')
+    
+    #pl.xlabel(keys[0])
+    #pl.ylabel(keys[1])
+
+    # Plot also the training points
+    #pl.scatter(X[:,0], X[:,1], c=y)
+    
+    plot_2d_histo_raw(X[:,0], X[:,1], y, keys[0], keys[1], x_max-x_min, y_max-y_min)
+
+    pl.title(title)
+
+    pl.axis('tight')
+    pl.show()
+
+def classify(X, y, keys):
+    print 'classify(X=%s, y=%s, keys=%s)' % (X.shape, y.shape, keys)
+
+    Xr, yr = select_features.resample_equal_y(X, y, 1.0)
+    print 'classify: Xr=%s, yr=%s' % (Xr.shape, yr.shape)
+    n_samples = Xr.shape[0]
+
+    if False:
+        X_train, y_train = Xr[:n_samples/2,:], yr[:n_samples/2]
+        X_test, y_test = Xr[n_samples/2:,:], yr[n_samples/2:]
+
+    NUM_FOLDS = 5
+    skf = StratifiedKFold(yr, NUM_FOLDS)
+
+    verbose = False
+
+    if verbose:
+        def P(s): print s
+    else:
+        def P(s): pass
+
+    n_iter_val = 500
+    for power_t_val in [0.9]:
+        for alpha_val in [0.1]: 
+            y_test_all = np.zeros(0)
+            y_pred_all = np.zeros(0)
+            for i,(train, test) in enumerate(skf):
+                X_train, y_train = Xr[train,:], yr[train]
+                X_test, y_test = Xr[test,:], yr[test]
+            
+                if verbose: common.SUBHEADING()
+                P('Fold %d of %d' % (i, NUM_FOLDS))
+                P('classify: X_train=%s, y_train=%s' % (X_train.shape, y_train.shape))
+                P('classify:  X_test=%s,  y_test=%s' % (X_test.shape, y_test.shape))
+
+                # fit the model
+                classifier = SGDClassifier(loss="hinge", alpha=alpha_val,  
+                    n_iter=n_iter_val, fit_intercept=True)
+           
+                classifier.fit(X_train, y_train)
+                y_pred = classifier.predict(X_test)
+
+                P('Classification report for classifier %s:\n%s\n' % (classifier, 
+                    metrics.classification_report(y_test, y_pred)))
+                P('Confusion matrix:\n%s' % metrics.confusion_matrix(y_test, y_pred))
+                
+                y_test_all = np.r_[y_test_all, y_test]
+                y_pred_all = np.r_[y_pred_all, y_pred]
+            
+            common.HEADING()
+            print 'Classification report for all %s:\n%s\n' % (
+                    classifier, metrics.classification_report(y_test_all, y_pred_all))
+            print 'Confusion matrix:\n%s' % metrics.confusion_matrix(y_test_all, y_pred_all)
+
+            # plot the line, the points, and the nearest vectors to the plane
+            fac = 1.0
+            print 'Downsampling by a further factor of %f' % fac
+            X_r, y_r = sklearn.utils.resample(X, y, n_samples = int(X.shape[0] * fac)) 
+            y_pred = classifier.predict(X)
+            plot_classification(X_r, y_r, y_pred, keys, 'Test', classifier)
+    exit()
+
+def predict(X, y, keys):
+    
+    print 'predict(%s, %s)' % (X.shape, y.shape, keys)
+    
+    # Run classifier with crossvalidation and plot ROC curves
+    cv = StratifiedKFold(y, k=6)
+    classifier = svm.SVC(kernel='linear', probability=True)
+    
+    print 'got classifier'
+
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+    all_tpr = []
+
+    for i, (train, test) in enumerate(cv):
+        print '%d: about to fit' % i
+        probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
+        print '%d: done fitting' % i
+        
+        # Compute ROC curve and area the curve
+        fpr, tpr, thresholds = roc_curve(y[test], probas_[:,1])
+        print '%d: got ROC' % i
+        mean_tpr += interp(mean_fpr, fpr, tpr)
+        mean_tpr[0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        print '%d: got AUC' % i
+        pl.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+
+        pl.plot([0, 1], [0, 1], '--', color=(0.6,0.6,0.6), label='Luck')
+
+        mean_tpr /= len(cv)
+        mean_tpr[-1] = 1.0
+        mean_auc = auc(mean_fpr, mean_tpr)
+        pl.plot(mean_fpr, mean_tpr, 'k--', label='Mean ROC (area = %0.2f)' % mean_auc, lw=2)
+
+        pl.xlim([-0.05,1.05])
+        pl.ylim([-0.05,1.05])
+        pl.xlabel('False Positive Rate')
+        pl.ylabel('True Positive Rate')
+        pl.title('Receiver operating characteristic example')
+        pl.legend(loc="lower right")
+        pl.show()
